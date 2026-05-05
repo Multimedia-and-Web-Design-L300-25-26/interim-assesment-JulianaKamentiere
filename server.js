98e2cc6309693b05e532fdfdaef3b63bde269161
Juliana Kamentiere <kamentierejuliana@gmail.com>
2026-05-05 00:34:00 +0000

    updated server.js

--- server.js
+++ server.js
@@ -9,13 +9,14 @@ const fs = require('fs');
 const app = express();
 
 app.use(cors({ 
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5000'],
+  origin: [
+    'https://juliana-crypto.netlify.app',
+    'http://localhost:5173'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.use(express.json());
-app.use(cookieParser());
 
 
 const publicDir = path.join(__dirname, 'public');
